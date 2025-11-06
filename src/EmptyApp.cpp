6a0d6cc6cc83b3439456b42106d3b7a56d3d7e00
Kiran George <gaurdian.kiran@gmail.com>
2025-11-06 13:59:29 +0100

    Engine update

--- src/EmptyApp.cpp
+++ src/EmptyApp.cpp
@@ -1,8 +1,12 @@
 ﻿#include "EmptyApp.h"
+#include "Core/ECS/Entity.h"
 
 void EmptyApp::Begin()
 {
     m_MainScene = new Core::Scene();
+    Core::ECS::Entity* testEntity = m_MainScene->CreateEntity();
+
+    testEntity->AddComponent<int>();
 }
 
 void EmptyApp::Update()
