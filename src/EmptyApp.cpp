1893360ba67dca60e5632965e54d831a1626c510
Kiran George <gaurdian.kiran@gmail.com>
2025-11-10 20:00:22 +0100

    Redoing ECS

--- src/EmptyApp.cpp
+++ src/EmptyApp.cpp
@@ -1,12 +1,7 @@
 ﻿#include "EmptyApp.h"
-#include "Core/ECS/Entity.h"
 
 void EmptyApp::Begin()
 {
-    m_MainScene = new Core::Scene();
-    Core::ECS::Entity* testEntity = m_MainScene->CreateEntity();
-
-    testEntity->AddComponent<int>();
 }
 
 void EmptyApp::Update()
