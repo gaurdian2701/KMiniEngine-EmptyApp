3e9d8a4c90751931a95a29621c2f868e3b0c9223
Kiran George <gaurdian.kiran@gmail.com>
2025-11-17 20:09:33 +0100

    Engine update

--- src/EmptyApp.cpp
+++ src/EmptyApp.cpp
@@ -1,8 +1,14 @@
 ﻿#include "EmptyApp.h"
+#include "Core/ECS/Components/Transform.h"
+#include "Scene/GameObject.h"
 
 void EmptyApp::Begin()
 {
     m_MainScene = new Core::GameScene(100);
+
+    Scene::GameObject* gameObject = m_MainScene->CreateGameObject();
+    gameObject->AddComponent<Core::ECS::Components::Transform>();
+    gameObject->RemoveComponent<Core::ECS::Components::Transform>();
 }
 
 void EmptyApp::Update()
