f967a50bc37e26ff810710f61930d0ffc3b28398
Kiran George <gaurdian.kiran@gmail.com>
2025-11-18 18:52:38 +0100

    Engine update

--- src/EmptyApp.cpp
+++ src/EmptyApp.cpp
@@ -1,4 +1,5 @@
 ﻿#include "EmptyApp.h"
+#include "Core/ECS/Components/Rigidbody.h"
 #include "Core/ECS/Components/Transform.h"
 #include "Scene/GameObject.h"
 
@@ -6,9 +7,21 @@ void EmptyApp::Begin()
 {
     m_MainScene = new Core::GameScene(100);
 
-    Scene::GameObject* gameObject = m_MainScene->CreateGameObject();
-    gameObject->AddComponent<Core::ECS::Components::Transform>();
-    gameObject->RemoveComponent<Core::ECS::Components::Transform>();
+    Scene::GameObject* gameObject1 = m_MainScene->CreateGameObject();
+    Scene::GameObject* gameObject2 = m_MainScene->CreateGameObject();
+    Scene::GameObject* gameObject3 = m_MainScene->CreateGameObject();
+
+    gameObject1->AddComponent<Core::ECS::Components::Transform>();
+    gameObject1->AddComponent<Core::ECS::Components::Rigidbody>();
+    gameObject2->AddComponent<Core::ECS::Components::Transform>();
+    gameObject3->AddComponent<Core::ECS::Components::Rigidbody>();
+    gameObject3->AddComponent<Core::ECS::Components::Transform>();
+
+    auto& array = m_MainScene->GetECSManager().GetSmallestEntityArray<Core::ECS::Components::Transform>();
+
+    gameObject1->RemoveComponent<Core::ECS::Components::Transform>();
+
+    delete gameObject1;
 }
 
 void EmptyApp::Update()
